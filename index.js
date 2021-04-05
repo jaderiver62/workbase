6f50833eb3f6e05f1831bd20cb4cf37c4c781b23
Nina Jade <jaderiver64@gmail.com>
2021-04-05 16:49:41 -0500

    refactoring several files, renamed userPrompt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const accessDatabase = require("./db/db.js");
-const inquirerPrompt = require("./lib/UserPrompt.js");
+const inquirerPrompt = require("./lib/userPrompt.js");
 const { username, pwd } = require("./utils/info.js").getInfo();
 
 const connection = mysql.createConnection({
